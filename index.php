9be3c69047a8fcd5d0b4f8fcc3a00a0566810a17
Wagner Nunes <wagner@wagner.(none)>
2013-11-22 12:43:53 -0200

    Comitando alterações

--- index.php
+++ index.php
@@ -16,23 +16,48 @@ $total = mysql_query("SELECT count(*) as qtd from paises");
 
 /*Se encontrou o total*/
 if($t = mysql_fetch_assoc($total)){
-	$n = $t['qtd']; // Total de regstros
-	$limite = 10; // Limite máximo de registros por página
-	$paginas = round($n/$limite); // Quantidade de páginas, que é basicamente, a quantidade de agrupamentos possíveis com o total de  registros que temos
-	$pagina = empty($_REQUEST['pagina'])?1:$_REQUEST['pagina']; /*Caso, na requisição, nenhuma página for especificada, será usada o valor padrão 1. Ou seja, obrigatoriamente estará na primeira página*/
-	$inicio = $pagina * $limite; /*Com base na página, é definido o registro de início para fazer a consulta*/ 
-	$sql = "SELECT * from paises limit $inicio,$limite";
+	
+	// Total de regstros
+	$n = $t['qtd']; 
+	
+	// Limite máximo de registros por página
+	$limite = 10; 
+	/*
+	 * A função round basicamente retorna, caso o número seja quebrado, o menor valor inteiro
+	 * Retorna Quantidade de páginas, que é basicamente, a quantidade de agrupamentos possíveis com o total de  registros que temos
+	 * Esta quantidade é obtida dividindo-se o total de registros pelo limite por página
+	 * */
+	$paginas = round($n/$limite); 
+	
+	/*Caso, na requisição, nenhuma página for especificada, será usada o valor padrão 1. Ou seja, obrigatoriamente estará na primeira página*/
+	$pagina = empty($_REQUEST['pagina'])?1:$_REQUEST['pagina']; 
+	
+	/*Com base na página, é definido o registro de início para fazer a consulta*/
+	$inicio = $pagina * $limite;  
+	
+	$sql = "SELECT * from paises limit $inicio,$limite"; /*Fazendo a consulta */
+	
 	$dados = mysql_query($sql);
-
+	
+	
+	/*Montando a apresentação*/
 	$saida = "<html><head><title>SCRIPT DE TESTE</title></head><body>";
 	$saida .= "<table border='1'><tr><th colspan='2'>PAÍS</th></tr>";
+	
 	/*Percorrendo todos os registros*/
 	while($d = mysql_fetch_assoc($dados)){
 		$saida .="<tr ><td colspan='2'>".$d['nome']."</td></tr>";
 	}
 	
-	/*INcluindo paginação simples: Se houver próxima,, exibirá o link Próxima. Se houver página anterior, exibirá Anterior. Sendo assim, os dois links não são exibidos na primeira e nem na ultima página*/
 	$saida .= "<tr>";
+	/*INcluindo paginação simples: Se houver próxima,, exibirá o link Próxima. Se houver página anterior, exibirá Anterior. Sendo assim, os dois links não são exibidos na primeira e nem na ultima página*/
+	
+	/*-----------------------------------RODAPÉ---------------------------------*/
+	/*
+	 * Caso a página atual não seja 1, mas um valor menor, aparecerá o link para a página anterior
+	 * */
+		/*Caso a página não tenha chegado na última, haverá um link para a próxima página*/
+	
 	if($pagina-1 > 0){
 		$saida .= "<td><a href='?pagina=".($pagina - 1 )."'>Anterior</a></td>";	
 	}else{
@@ -44,6 +69,19 @@ if($t = mysql_fetch_assoc($total)){
 	}else{
 		$saida .= "<td>Está na última</td>";
 	}
+	
+	/*-----------------------------------FECHANDO O RODAPÉ---------------------------------*/
+	
+	/*
+	 * Caso voçê queira uma paginação diferente, tipo, em que o usuário pode selecionar uma determinada página diretamente, é só comentar o trecho do rodapé acima e descomentar o trecho abaixo
+	 * */
+	/*-----------------------------------RODAPÉ---------------------------------*/
+	/*$saida .= "<td colspan='2'>";
+	for($x=1;$x<=$paginas;$x++){
+		$saida .= ($pagina == $x?" <b> ":" ")."<a href ='?pagina=$x'>".$x."</a>".($pagina == $x?" </b> ":" ");
+	}
+	$saida .= "</td>";*/
+	/*-----------------------------------FECHANDO O RODAPÉ---------------------------------*/
 	$saida .= "</tr>";
 	$saida .= "</table></body></html>";
 }
